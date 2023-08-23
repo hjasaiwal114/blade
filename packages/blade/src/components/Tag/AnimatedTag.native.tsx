058e15e3dae467c54b1d90f5175bb1d64636bc13
Saurabh Daware <saurabh.daware@razorpay.com>
2023-08-23 10:34:54 +0530

    commit failed code

--- packages/blade/src/components/Tag/AnimatedTag.native.tsx
+++ packages/blade/src/components/Tag/AnimatedTag.native.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import Animated, {
-  Easing,
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  runOnJS,
-} from 'react-native-reanimated';
+import type { EasingFn } from 'react-native-reanimated';
+import Animated, { runOnJS, Keyframe } from 'react-native-reanimated';
+import type { ReanimatedKeyframe } from 'react-native-reanimated/lib/typescript/reanimated2/layoutReanimation/animationBuilder/Keyframe';
 import { Tag } from './Tag';
 import {
   TAG_MAX_WIDTH_END,
@@ -14,48 +10,41 @@ import {
   TAG_OPACITY_START,
 } from './tagAnimationConfig';
 import type { AnimatedTagProps } from './types';
-import { castNativeType, useTheme } from '~utils';
+import { useTheme } from '~utils';
 
-const useAnimatedTag = (
-  onAnimationEndCallback: () => void,
-): {
-  animate: () => void;
-  animatedStyle: {
-    maxWidth: number;
-    opacity: number;
-  };
+const useAnimatedTag = (): {
+  entering: ReanimatedKeyframe;
+  exiting: ReanimatedKeyframe;
 } => {
-  const maxWidth = useSharedValue(TAG_MAX_WIDTH_START); // Initial max-width value
-  const opacity = useSharedValue(TAG_OPACITY_START); // Initial opacity value
   const { theme } = useTheme();
 
-  const animate = (): void => {
-    maxWidth.value = withTiming(
-      TAG_MAX_WIDTH_END,
-      {
-        duration: theme.motion.duration.xquick,
-        easing: castNativeType(theme.motion.easing.exit.effective),
-      },
-      (isComplete) => {
-        if (isComplete) {
-          runOnJS(onAnimationEndCallback)();
-        }
-      },
-    );
-    opacity.value = withTiming(TAG_OPACITY_END, {
-      duration: theme.motion.duration.xquick,
-      easing: Easing.out(Easing.exp),
-    });
-  };
+  const duration = theme.motion.duration.xquick;
+  const entranceEasing = (theme.motion.easing.entrance.effective as unknown) as EasingFn;
+  const exitEasing = (theme.motion.easing.exit.effective as unknown) as EasingFn;
 
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      maxWidth: maxWidth.value,
-      opacity: opacity.value,
-    };
-  });
+  const exiting = new Keyframe({
+    0: {
+      opacity: TAG_OPACITY_START,
+      maxWidth: TAG_MAX_WIDTH_START,
+    },
+    100: {
+      opacity: TAG_OPACITY_END,
+      maxWidth: TAG_MAX_WIDTH_END,
+      easing: exitEasing,
+    },
+  }).duration(5000);
 
-  return { animate, animatedStyle };
+  const entering = new Keyframe({
+    0: {
+      opacity: TAG_OPACITY_END,
+    },
+    100: {
+      opacity: TAG_OPACITY_START,
+      easing: entranceEasing,
+    },
+  }).duration(duration);
+
+  return { entering, exiting };
 };
 
 const AnimatedTag = ({
@@ -63,20 +52,28 @@ const AnimatedTag = ({
   currentTagIndex,
   activeTagIndex,
   onDismiss,
-}: AnimatedTagProps): React.ReactElement => {
-  const onAnimationEnd = (): void => {
-    onDismiss({ tagIndex: currentTagIndex, tagName: children });
-  };
+}: AnimatedTagProps): React.ReactElement | null => {
+  const [isTagVisible, setIsTagVisible] = React.useState(true);
+  const { entering, exiting } = useAnimatedTag();
 
-  const { animate, animatedStyle } = useAnimatedTag(onAnimationEnd);
+  // const onAnimationEnd = (isFinished: boolean): void => {
+  //   'worklet';
+  //   console.log('onAnimationEnd', { currentTagIndex, children, isFinished });
+  //   if (isFinished && !isTagVisible) {
+  //     runOnJS(onDismiss)({ tagIndex: currentTagIndexRef.current, tagName: currentTagName.current });
+  //   }
+  // };
 
-  return (
-    <Animated.View style={animatedStyle}>
+  // const exitingWithCallback = exiting.withCallback(onAnimationEnd);
+
+  return isTagVisible ? (
+    <Animated.View entering={entering} exiting={exiting}>
       <Tag
         _isVirtuallyFocussed={currentTagIndex === activeTagIndex}
         _isTagInsideInput={true}
         onDismiss={() => {
-          animate();
+          setIsTagVisible(false);
+          onDismiss({ tagIndex: currentTagIndex, tagName: children });
         }}
         marginRight="spacing.3"
         marginY="spacing.2"
@@ -84,7 +81,7 @@ const AnimatedTag = ({
         {children}
       </Tag>
     </Animated.View>
-  );
+  ) : null;
 };
 
 export { AnimatedTag };
